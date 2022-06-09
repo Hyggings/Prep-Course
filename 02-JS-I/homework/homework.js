57fa33ccdc867bb4be43955abde05410cedb3065
Hyggings <jjosenciso@gmail.com>
2022-06-09 09:15:23 -0500

    I found a diferent way to write-strings length exercise

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,7 +71,9 @@ function tienenMismaLongitud(str1, str2) {
    // Devuelve "true" si las dos strings tienen la misma longitud
    // De lo contrario, devuelve "false"
    // Tu código:
-  return str1.length === str2.length;
+  if (str1.length === str2.length){
+    return true;
+  } return false;
 }
 
 function menosQueNoventa(num) {
